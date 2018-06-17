54a0cf229f0ee0eada5bba3e585bc62189ef8de7
oooofeiger <101457769@qq.com>
2018-06-17 21:01:17 +0800

    modified server.js and ecosystom.json

--- build/server.js
+++ build/server.js
@@ -4,7 +4,7 @@ var app = new express();
 
 app.use(express.static(__dirname))
 app.use('*',function(req,res){
-  res.sendFile(path.resolve(__dirname,'build/index.html'))
+  res.sendFile(path.resolve(__dirname,'index.html'))
 })
 app.listen(8090);
 console.log('app is running at port 8090')
